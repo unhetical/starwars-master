e1979cd3363cd413e635da9260c4d40d78fe62c4
unhetical <unhetical69@hotmail.com>
2020-10-05 17:28:54 +0200

    refactor styles to scss components

--- src/app/shared/components/detail-dialog/detail-dialog.component.ts
+++ src/app/shared/components/detail-dialog/detail-dialog.component.ts
@@ -1,6 +1,5 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
-import { Ship } from '../../models/ship.interface';
 
 @Component({
   templateUrl: './detail-dialog.component.html',
